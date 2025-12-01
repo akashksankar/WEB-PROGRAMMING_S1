1a524399a15c74ab22a6cf6841a50a4f844aeec5
Akash sankar <skygfxlord@gmail.com>
2025-12-01 06:18:24 +0530

    initial committ
    Removed old player list and replaced with a new title.

--- program 15/cricket.php
+++ program 15/cricket.php
@@ -1,36 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Cricket Players List</title>
-</head>
-<body>
-
-<h2>Indian Cricket Players</h2>
-
-<?php
-$players = array("Virat Kohli", "Rohit Sharma", "MS Dhoni", "Hardik Pandya", "Jasprit Bumrah");
-
-echo "<table border='1' cellpadding='8' cellspacing='0'>";
-echo "<tr><th>Player Name</th></tr>";
-
-foreach ($players as $player) {
-    echo "<tr>";
-    echo "<td>$player</td>";
-    echo "</tr>";
-}
-
-echo "</table>";
-?>
-
-</body>
-</html>
-  
-  
-  
-  
-  <!DOCTYPE html>
-<html>
-<head>
     <title>Indian Cricket Players</title>
     <style>
         table {
